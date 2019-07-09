aba2d2937cb134779b0b849c7d4633d90b7433bc
m1ngky <52159712+m1ngky@users.noreply.github.com>
2019-07-09 17:19:22 +0900

    0709/템플릿적용까지했습니다.

---
+++ chachacha/src/main/java/c/h/a/controller/HomeController.java
@@ -0,0 +1,21 @@
+package c.h.a.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value = "/home")
+	public String home() {
+		
+		
+		System.out.println("home¿‘¥œ¥Ÿ.");
+		
+		return "home";
+	}
+	
+}
