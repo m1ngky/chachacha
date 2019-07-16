cfc6ebf84ffd82e09426df018cc1456cdab29ac3
user1 <user1@DESKTOP-8UC660Q>
2019-07-16 11:11:09 +0900

    보드.sql 테스트

--- chachacha/src/main/resources/sql/Board.sql
+++ chachacha/src/main/resources/sql/Board.sql
@@ -13,9 +13,9 @@ create table Fboard(
    CONTENT   VARCHAR2(4000),            --내용
    PRICE     varchar2(20),            --가격  (판매가격,사는가격,공동전원가 구매가격)
    GPRICE    varchar2(20),            --공동구매 가격
-   FILEname      VARCHAR2(400),         --첨부될 파일 명(가공)
+   FILEname      VARCHAR2(400),        --첨부될 파일 명(가공)
    OriginalFile  VARCHAR2(50),         --첨부될 파일 명
-   InputDATE DATE,                  --글 작성 날짜 
+   InputDATE DATE,                   --글 작성 날짜 
    primary key(num),
    constraint fk_fboard_name foreign key(id) 
    references fmember(id) on delete cascade
